8a9eca57a99d35f5d216745b9619a10ffd5346b5
Johan van der Walt <joe@kohde.io>
2019-06-18 06:25:42 +0200

    Minor installation changes

--- enable.sh
+++ enable.sh
@@ -1,2 +1,5 @@
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/jelera/vim-javascript-syntax.git ~/.vim/bundle/vim-javascript-syntax
 cp -r .vim ~/
 cp .vimrc ~/
